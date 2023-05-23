43ae1224ab98fbb8094f06e47d5b536daabf1eab
Paul <pmacellaro7@aol.com>
2023-05-23 15:24:47 -0400

    yayyyyyyyyyyyyyyyyy

---
+++ src/00-objectball.js
@@ -0,0 +1,13 @@
+function gameObject() {
+    const gameStats= {
+        home: {
+        teamName: "",
+        colors: "blue",
+        players: {
+          "Alan Anderson":{number:0, Shoe:16, Points:22, Rebound:12, Assist:12, Steals:3, Blocks:1,SlamDunks:1,},
+          "Reggie Evans": {
+        }, away:"" }
+        
+}}
+return gameStats}
+console.log(gameObject())
