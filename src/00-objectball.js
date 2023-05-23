1f79a8013a40eabf42db6aa2d7d0b253ddd40618
Erica Belzer <belzer49@gmail.com>
2023-05-23 15:23:24 -0500

    finished away player stats

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,4 +1,4 @@
-function gameObject(){
+function gameObject() {
     let teams = {
         home:{
             teamName: "Brooklyn Nets",
@@ -13,15 +13,103 @@ function gameObject(){
                     steals: 3, 
                     blocks: 1,
                     slamDunks: 1
+                },
+                reggieEvans: {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7,
+                },
+                brookLopez: {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15
+                },
+                masonPlumlee: {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5
+                },
+                jasonTerry: {
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamDunks: 1
                 }
             }
         }
         , away: {
             teamName: "Charlotte Hornets",
-            colors: ["Purple", "Turquoise"]
+            colors: ["Purple", "Turquoise"],
             players: {
-                
+                jeffAdrien: {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks:2
+                }
+                bismakBiyombo: {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10
+                }
+                desagnaDiop: {
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5
+                }
+                benGordon: {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0
+                }
+                brendanHaywood: {
+                    numnber: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12
+                }
             }
         }
     }
-}
