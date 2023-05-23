8e0d0ab3c46d6b2bcaf72dd95ca442253b843ef2
Erica Belzer <belzer49@gmail.com>
2023-05-23 14:57:57 -0500

    adding player keys

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,12 +1,27 @@
-function gameObject() {
-    const home = {
-        teamName: "Brooklyn Mets"
-        colors: ["Black", "White"]
-        players: []
-    }
-    const away = {
-        teamName:
-        colors:
-        players:
+function gameObject(){
+    let teams = {
+        home:{
+            teamName: "Brooklyn Nets",
+            colors: ["Black", "White"],
+            players: {
+                alanAnderson: {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3, 
+                    blocks: 1,
+                    slamDunks: 1
+                }
+            }
+        }
+        , away: {
+            teamName: "Charlotte Hornets",
+            colors: ["Purple", "Turquoise"]
+            players: {
+                
+            }
+        }
     }
 }
