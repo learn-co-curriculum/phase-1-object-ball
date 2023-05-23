a16b01186388d348d35c92e720531f3f5dfae14f
Erica Belzer <belzer49@gmail.com>
2023-05-23 14:31:08 -0500

    created function and keys

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,6 +1,12 @@
-function gameObject () {
-home : {
-    
-}
-away:
+function gameObject() {
+    const home = {
+        teamName: "Brooklyn Mets"
+        colors: ["Black", "White"]
+        players: []
+    }
+    const away = {
+        teamName:
+        colors:
+        players:
+    }
 }
