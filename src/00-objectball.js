45b0ec92693448a179ea39dde51d2b4555b6948c
Priyam Das <domidas01@gmail.com>
2023-05-23 15:14:51 -0400

    complete home team

---
+++ src/00-objectball.js
@@ -0,0 +1,66 @@
+function gameObject(){
+    const obj = {
+        "home": {
+            "teamName": "Brooklyn Nets",
+            "colors": ["Black", "White"],
+            "players" : {
+                "Alan Anderson":{
+                    "number": 0,
+                    "shoe": 16,
+                    "points": 22,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 3,
+                    "blocks": 1,
+                    "slamDunks": 1
+                },
+                "Reggie Evans":{
+                    "number": 30,
+                    "shoe": 14,
+                    "points": 12,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 12,
+                    "blocks": 12,
+                    "slamDunks": 7
+                },
+                "Brook Lopez":{
+                    "number": 11,
+                    "shoe": 17,
+                    "points": 17,
+                    "rebounds": 19,
+                    "assists": 10,
+                    "steals": 3,
+                    "blocks": 1,
+                    "slamDunks": 15
+                },
+                "Mason Plumlee":{
+                    "number": 1,
+                    "shoe": 19,
+                    "points": 26,
+                    "rebounds": 12,
+                    "assists": 6,
+                    "steals": 3,
+                    "blocks": 8,
+                    "slamDunks": 5
+                },
+                "Jason Terry":{
+                    "number": 31,
+                    "shoe": 15,
+                    "points": 19,
+                    "rebounds": 2,
+                    "assists": 2,
+                    "steals": 4,
+                    "blocks": 11,
+                    "slamDunks": 1
+                },
+
+            }
+        },
+        "away": {
+            "teamName": ,
+            "colors": ,
+            "players" 
+        }
+    }
+}
