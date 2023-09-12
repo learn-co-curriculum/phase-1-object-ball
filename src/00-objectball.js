f486d29808a20a4b2f30d653b5957d1d54e449d4
LingZhouChen <lzcciao@gmail.com>
2023-09-12 16:19:03 -0400

    added away team stats

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -5,14 +5,14 @@ function gameObject() {
             colors: ["Black", "White"],
             players: {
                 "Alan Anderson": {
-                        number: 0,
-                        shoe: 16,
-                        points: 22,
-                        rebounds: 12,
-                        assists: 12,
-                        steals: 3,
-                        blocks: 1,
-                        slamDunks: 1
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1
                 },
                 "Reggie Evans": {
                     number: 30,
@@ -57,18 +57,63 @@ function gameObject() {
             }
         },
         away: {
-            teamName: ,
-            colors: ,
+            teamName: "Charlotte Hornets",
+            colors: ["Turquoise", "Purple"],
             players: {
-                number: ,
-                shoe: ,
-                points: ,
-                rebounds: ,
-                assists: ,
-                steals: ,
-                blocks: ,
-                slamDunks:
+                "Jeff Adrien": {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2
+                },
+                "Bismak Biyombo": {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10
+              },
+                "DeSagna Diop": {
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5
+                },  
+                "Ben Gordon": {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0
+                },  
+                "Brendan Haywood": {
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12
+                },  
             }
         }    
     }
+    return stats;
 }
+
+console.log(gameObject());
